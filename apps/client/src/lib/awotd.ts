c395ad336ff1d087605049c29f50b032e77d81d7
Cristian <c.isaacdominguez@gmail.com>
2025-06-03 23:24:52 -0700

    create temporary metadata for artwork

---
+++ apps/client/src/lib/awotd.ts
@@ -0,0 +1,8 @@
+import Picture from "../assets/a_bear_walking.png";
+
+export const AWOTD = {
+  title: "A Bear Walking",
+  artist: "Leonardo da Vinci",
+  image: Picture,
+  referred_year: "ca. 1482-85",
+};
