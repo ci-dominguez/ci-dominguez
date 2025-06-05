b52431cfc887560e85646a72e6940e5be2d44b54
Cristian <c.isaacdominguez@gmail.com>
2025-06-04 23:54:34 -0700

    add local navigation assets

---
+++ apps/client/src/lib/social-links.ts
@@ -0,0 +1,18 @@
+export const SOCIALS = [
+  {
+    href: 'mailto:cdom27@outlook.com',
+    text: 'cdom27@outlook.com',
+  },
+  {
+    href: 'https://linkedin.com/in/cdom27',
+    text: 'LinkedIn',
+  },
+  {
+    href: 'https://github.com/cdom27',
+    text: 'GitHub',
+  },
+  {
+    href: '/assets/cristian-dominguez-resume.pdf',
+    text: 'Resume',
+  },
+];
