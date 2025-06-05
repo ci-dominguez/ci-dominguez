3632ecc20c865f6498497e490b3fd3c958a5b5bb
Cristian <c.isaacdominguez@gmail.com>
2025-06-04 23:54:49 -0700

    create footer with local data

--- apps/client/src/components/layouts/site-layout.tsx
+++ apps/client/src/components/layouts/site-layout.tsx
@@ -1,3 +1,8 @@
+import { Link } from 'react-router';
+import Logo from '../../assets/logo.svg';
+import ExternalLink from '../../assets/link.svg';
+import { SOCIALS } from '../../lib/social-links';
+
 interface SiteLayoutProps {
   children: React.ReactNode;
 }
@@ -9,7 +14,41 @@ const SiteLayout = ({ children }: SiteLayoutProps) => {
         <nav>Start</nav>
       </header> */}
       <main className='min-h-screen scroll-smooth'>{children}</main>
-      <footer className='px-4'>end</footer>
+
+      <footer className='px-4 pb-28'>
+        <Link to='/' className='flex items-center gap-2'>
+          <img src={Logo} alt='Logo' className='size-6' />
+          <span className='font-semibold'>Cristian Dominguez</span>
+        </Link>
+
+        <p className='mt-2.5'>I enjoy building things on the web.</p>
+
+        <address className='flex flex-col gap-2 mt-8 not-italic'>
+          <h4 className='font-semibold'>Connect With Me</h4>
+          <ul className='flex flex-col gap-2'>
+            {SOCIALS.map((s, index) => {
+              return (
+                <li key={index} className='self-start'>
+                  <a
+                    href={s.href}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='flex items-center gap-2 hover:underline'
+                  >
+                    <span>{s.text}</span>
+                    <img
+                      src={ExternalLink}
+                      alt=''
+                      aria-hidden='true'
+                      className='w-4'
+                    />
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </address>
+      </footer>
     </>
   );
 };
