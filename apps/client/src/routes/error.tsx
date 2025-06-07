ce93690b22c67b0e62380eef02a5a0c0c4b2bcbe
Cristian <c.isaacdominguez@gmail.com>
2025-06-06 22:25:51 -0700

    temporarily update 404 page for testing

--- apps/client/src/routes/error.tsx
+++ apps/client/src/routes/error.tsx
@@ -1,7 +1,15 @@
-import SiteLayout from "../components/layouts/site-layout";
+import SiteLayout from '../components/layouts/site-layout';
 
 const Error = () => {
-  return <SiteLayout>404</SiteLayout>;
+  return (
+    <SiteLayout>
+      <section className='bg-bg-dark text-bg-light py-28 lg:py-50'>
+        <div className='px-4 xs:px-10 lg:px-20 w-full max-w-[1365px] mx-auto flex flex-col gap-14'>
+          <h1>404</h1>
+        </div>
+      </section>
+    </SiteLayout>
+  );
 };
 
 export default Error;
