2ccca958ba56600112068e9c4601c19f5989901b
Cristian <c.isaacdominguez@gmail.com>
2025-06-07 23:04:32 -0700

    start about page and update router

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import ObserverProvider from './providers/observer-provider';
 import Landing from './routes/landing';
+import About from './routes/about';
 import Error from './routes/error';
 import './index.css';
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <ObserverProvider>
         <Routes>
           <Route path='/' element={<Landing />} />
+          <Route path='/about' element={<About />} />
           <Route path='*' element={<Error />} />
         </Routes>
       </ObserverProvider>
---
+++ apps/client/src/routes/about.tsx
@@ -0,0 +1,11 @@
+import SiteLayout from '../components/layouts/site-layout';
+
+const About = () => {
+  return (
+    <SiteLayout>
+      <h1>all about me or something</h1>
+    </SiteLayout>
+  );
+};
+
+export default About;
